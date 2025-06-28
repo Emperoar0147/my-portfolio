1209f8e85ff9010c5b88bc0f4d5422caada34381
Emperoar0147 <138697215+Emperoar0147@users.noreply.github.com>
2025-06-28 19:43:02 +0800

    Fix: Switch to HashRouter for GitHub Pages

--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
 import { HashRouter } from "react-router-dom";
+import App from "./App";
+import "./index.css";
 
-const root = ReactDOM.createRoot(...);
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <HashRouter>
