59f6ecee0ac783f44f52b1db1b42f6a2f9a5d3ff
Emperoar0147 <138697215+Emperoar0147@users.noreply.github.com>
2025-06-28 20:05:36 +0800

    Test rendering minimal components

--- src/App.js
+++ src/App.js
@@ -1,28 +1,12 @@
 import React from "react";
-import Banner from "./components/banner/Banner";
-import Contact from "./components/contact/Contact";
-import Features from "./components/features/Features";
-import Footer from "./components/footer/Footer";
-import FooterBottom from "./components/footer/FooterBottom";
 import Navbar from "./components/navbar/Navbar";
-import Projects from "./components/projects/Projects";
-import Resume from "./components/resume/Resume";
-import Testimonial from "./components/testimonial/Testimonial"; // Double-check spelling
 
 function App() {
   return (
-    <div className="w-full h-auto bg-bodyColor text-lightText px-4">
+    <div style={{ textAlign: "center", marginTop: "2rem" }}>
       <Navbar />
-      <div className="max-w-screen-xl mx-auto">
-        <Banner />
-        <Features />
-        <Projects />
-        <Resume />
-        <Testimonial />
-        <Contact />
-        <Footer />
-        <FooterBottom />
-      </div>
+      <h1>Render Test</h1>
+      <p>If you can see this, React is working.</p>
     </div>
   );
 }
